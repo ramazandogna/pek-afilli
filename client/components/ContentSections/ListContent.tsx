35972daed500380bed24714de92cd62f708075a1
Ramazan DOĞAN <doganrmzn40@gmail.com>
2024-07-06 14:31:19 +0300

    Folder Names

---
+++ client/components/ContentSections/ListContent.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image';
+import Card from '../card/index';
+import Images from '../../helpers/slider/images';
+import Link from 'next/link';
+
+export default function ListContent({ posts }: { posts: any[] }) {
+  if (!posts) {
+    return (
+      <Card>
+        <div className="flex h-[550px] w-full items-center justify-center bg-black/60">
+          Loading...
+        </div>
+      </Card>
+    );
+  }
+
+  function truncateText(text: string, wordLimit: number) {
+    const words = text.split(' ');
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(' ') + '..';
+    }
+    return text;
+  }
+
+  const listPosts = posts.slice(1, 4);
+
+  const truncatedTitle = truncateText(listPosts[0].title, 3);
+
+  return (
+    <Card>
+      <div className="mb-8 mt-2 w-full border-b-2">
+        <h2 className="mt-4 inline-flex border-b-[3px] border-[#0693e3] pb-3 text-[18px] font-bold text-[#0693e3] transition-all hover:border-[#0061b1] hover:text-[#0061b1]">
+          <Link
+            href={listPosts[1].title.toLowerCase().replace(/\s+/g, '-')}
+            className="block overflow-hidden text-ellipsis whitespace-nowrap"
+          >
+            {truncatedTitle}
+          </Link>
+        </h2>
+      </div>
+      <div className="flex h-auto w-full flex-col gap-[16px] md:h-[650px]">
+        <Link
+          href={Images[1].title.toLowerCase().replace(/\s+/g, '-')}
+          className="relative h-[375px] w-full overflow-hidden rounded md:h-[550px]"
+        >
+          <Image
+            src={Images[1].src}
+            alt={Images[1].alt}
+            loading="lazy"
+            sizes="(max-width: 768px) 100vw, 50vw"
+            fill
+            className="object-cover"
+          />
+          <div className=" absolute bottom-[16px] left-[16px] z-10 mr-[16px] overflow-hidden bg-black/30 px-2 py-[2px] text-[20px] text-white">
+            {posts[1].title}
+          </div>
+        </Link>
+        <div className="flex h-[550px] w-full flex-col gap-[24px] rounded ">
+          {listPosts.map((post) => (
+            <div
+              className="flex h-[70px] min-h-[70px] flex-1  grow gap-[8px] md:h-auto"
+              key={post.id}
+            >
+              <Link
+                href={post.title.toLowerCase().replace(/\s+/g, '-')}
+                className="relative flex h-full w-[110px] min-w-[110px] items-start rounded"
+              >
+                <Image
+                  src={Images[2].src}
+                  alt={Images[2].alt}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  loading="lazy"
+                  fill
+                  className="rounded object-cover"
+                />
+              </Link>
+              <Link
+                href={post.title.toLowerCase().replace(/\s+/g, '-')}
+                className="font-700 grow text-[14px] transition-all hover:text-[#0693e3]"
+              >
+                <h2>{post.title}</h2>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Card>
+  );
+}
---
+++ client/layouts/Header/index.tsx
@@ -0,0 +1,80 @@
+//Styles
+import styles from './header.module.css';
+//icons
+import { InstagramIcon } from '../../icons/InstagramIcon';
+import { PinterestIcon } from '../../icons/PinterestIcon';
+import { TwitterIcon } from '../../icons/TwitterIcon';
+import { YoutubeIcon } from '../../icons/YoutubeIcon';
+import Link from 'next/link';
+import { SearchIcon } from '../../icons/SearchIcon';
+import { SignInIcon } from '../../icons/SignInIcon';
+import { SignUpIcon } from '../../icons/SignUpIcon';
+import { HotIcon } from '../../icons/HotIcon';
+
+export default function Header() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={`${styles.wrapper} width-container`}>
+        <Link href="/">
+          <div className={`${styles.logo} backgroundContent`}>
+            <div>PEK</div>
+            <div>AFİLLİ</div>
+          </div>
+        </Link>
+        <div className={styles.menus}>
+          <div className={styles.topMenu}>
+            <div className={styles.topLeft}>
+              <div className={styles.social}>
+                <div className={styles.iconBg}>
+                  <InstagramIcon className={styles.icon} />
+                </div>
+                <div className={styles.iconBg}>
+                  <TwitterIcon className={styles.icon} />
+                </div>
+                <div className={styles.iconBg}>
+                  <YoutubeIcon className={styles.icon} />
+                </div>
+                <div className={styles.iconBg}>
+                  <PinterestIcon className={styles.icon} />
+                </div>
+              </div>
+              <div className={styles.inputWrapper}>
+                <input
+                  type="text"
+                  placeholder="Search in the site.."
+                  className={styles.searchInput}
+                />
+                <div className={styles.inputIcons}>
+                  <div className={styles.inputBorder}></div>
+                  <SearchIcon className={styles.inputIcon} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.topRight}>
+              <Link href="/auth/login" className={`${styles.topRightSignIn} backgroundContent`}>
+                <SignInIcon className={styles.signInIcon} />
+                <div>Sign In</div>
+              </Link>
+              <Link href="/auth/register" className={`${styles.topRightSignUp} backgroundContent2`}>
+                <SignUpIcon className={styles.signUpIcon} />
+                <div>Join Now</div>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.bottomMenu}>
+            <ul className={styles.bottomLeft}>
+              <li className={styles.bottomLeftItem}>Magazine</li>
+              <li className={styles.bottomLeftItem}>Series</li>
+              <li className={styles.bottomLeftItem}>Technologies</li>
+              <li className={styles.bottomLeftItem}>Motivation</li>
+            </ul>
+            <Link href="/" className={styles.bottomRight}>
+              <HotIcon className={styles.hotIcon} />
+              <div>What's Hot</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
