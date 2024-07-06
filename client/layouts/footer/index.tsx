b021cf5ae608305bc16ff8ff2ba09d46d085b5e6
Ramazan DOĞAN <doganrmzn40@gmail.com>
2024-07-06 17:27:42 +0300

    Footer

--- client/layouts/footer/index.tsx
+++ client/layouts/footer/index.tsx
@@ -1,3 +1,28 @@
+import Link from 'next/link';
+import styles from './footer.module.css';
+
 export default function Footer() {
-  return <footer className="width-container">footer</footer>;
+  return (
+    <footer className="rounded bg-[#161619] py-[20px] text-[13px] text-white ">
+      <div className={`${styles.wrapper} width-container flex gap-4`}>
+        <div className="flex grow py-[10px] md:flex-[3] ">
+          © 2024 Pek Afilli tüm hakları saklıdır.
+        </div>
+        <ul className="ml-auto flex  items-center gap-2 ">
+          <Link className="hover:text-[#0693e3]" href="/">
+            <li>İletişim</li>
+          </Link>
+          <Link className="hover:text-[#0693e3]" href="/">
+            <li>Hakkımızda</li>
+          </Link>
+          <Link className="hover:text-[#0693e3]" href="/">
+            <li>Gizlilik Politikası</li>
+          </Link>
+          <Link className="hover:text-[#0693e3]" href="/">
+            <li>Ana Sayfa</li>
+          </Link>
+        </ul>
+      </div>
+    </footer>
+  );
 }
