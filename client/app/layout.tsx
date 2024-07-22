8511902a1dc15413ccdfa8129e92acdf5ac82f27
Ramazan DOĞAN <doganrmzn40@gmail.com>
2024-07-22 18:45:12 +0300

    uno.css to icons.css

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 import '../styles/hoverElements.css';
+import '../styles/icons.css'
 
 const inter = Inter({ subsets: ['latin'] });
 
