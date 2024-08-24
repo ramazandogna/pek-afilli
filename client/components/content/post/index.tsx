e9215e3982357ffe093f8c8424ffb5f5eadd1af9
RAMAZAN DOGAN <doganrmzn40@gmail.com>
2024-08-24 19:28:27 +0300

    domNode Type

--- client/components/content/post/index.tsx
+++ client/components/content/post/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import parse, { domToReact } from 'html-react-parser';
+import parse, { domToReact, Element, DOMNode } from 'html-react-parser';
 import PostImage from '../../image/postImage';
 
 export default function Post({ post }: { post: string }) {
-
   const defaultWidth = 718; // number
   const defaultHeight = 458; // number
 
   const options = {
-    replace: (domNode) => {
-      if (domNode.name === 'img') {
+    replace: (domNode: DOMNode) => {
+      // Tip kontrolü yaparak sadece Element türünü işleyelim
+      if (domNode.type === 'tag' && domNode instanceof Element && domNode.name === 'img') {
         const { src, alt } = domNode.attribs;
         return <PostImage src={src} alt={alt} layout="responsive" width={700} height={475} />;
       }
